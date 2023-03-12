2751f89798a061320b41ded0b489e5eb1779f2b6
luisana <11luisanalandaeta@gmail.com>
2023-03-12 09:26:49 -0300

    mi primer commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int i= 1;
+      ;while (i <=25);
+      System.out.println (i);
+      i++;
+        }
+
+        }
+
+
+
+
+
+
+
